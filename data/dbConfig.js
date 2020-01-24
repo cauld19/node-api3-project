2eedc5f4e3ecd383289ffd552e06157fb28a5de6
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-01-23 20:14:35 -0800

    Heroku set up

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,4 +1,6 @@
 const knex = require('knex');
-const knexConfig = require('../knexfile.js');
 
-module.exports = knex(knexConfig.development);
+const environment = 'development';
+const knexConfig = require('../knexfile.js')[environment];
+
+module.exports = knex(knexConfig);
--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+require('dotenv').config();
+
 const server = require('./server.js');
 
-server.listen(4000, () => {
-    console.log('listening on port 4000');
+const port = process.env.PORT || 5000;
+
+// server.set('port', (process.env.PORT || 5000));
+
+
+server.listen(port, () => {
+    console.log(`listening on port ${port}`);
 });
+
+
